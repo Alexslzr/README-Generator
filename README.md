0aa9c5ca409b50424c6dcb31b70ede508b782224
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-05-15 13:58:26 -0700

    final changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 
-# Hola
-
+# MyREADMEGenerator
+![License](https://img.shields.io/static/v1?label=license&message=APACHE+2.0&color=blue)
 ## Description
-uwu
+This project is intented to help users to quickly create proffesional readme files in a easy way via terminal
 ## Table of Contents
 * [Description](#description)
 * [Installation](#installation)
@@ -17,11 +17,11 @@ to install necessary dependencies, run the following command;
 npm i
 ```
 ## Usage
-yepa
+its open to freely use
 ## License
-This Project is under MIT license.
+This Project is under APACHE 2.0 license.
 ## Contributing
-asasas 
+to contribute in the project please contact the owner of the repo, which info its under the question section 
 ## Tests 
 To test the project please run the following command:
 ```
@@ -29,4 +29,4 @@ npm test
 ```
 ## Questions
 If you have any other question about the repo you can contact me via mail at alejandroslzr17@gmail.com<br>
-Get to know more of my work at [alexslzr](https://github.com/alexslzr)
+Get to know more of my work at my Github: [alexslzr](https://github.com/alexslzr)
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const questions = [
     {
       type: 'list',
       message: 'What license does your project have',
-      name: 'licence',
+      name: 'license',
       choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
     },
     {
@@ -52,10 +52,10 @@ const questions = [
     }
 ];
 
-const generateReadMe = ({title, description, installation, usage, contributing, tests, licence, github, mail}) =>
+const generateReadMe = ({title, description, installation, usage, contributing, tests, license, github, mail}) =>
 `
 # ${title}
-
+![License](https://img.shields.io/static/v1?label=license&message=${license.split(' ').join('+')}&color=blue)
 ## Description
 ${description}
 ## Table of Contents
@@ -74,7 +74,7 @@ ${installation}
 ## Usage
 ${usage}
 ## License
-This Project is under ${licence} license.
+This Project is under ${license} license.
 ## Contributing
 ${contributing} 
 ## Tests 
@@ -84,7 +84,7 @@ ${tests}
 \`\`\`
 ## Questions
 If you have any other question about the repo you can contact me via mail at ${mail}<br>
-Get to know more of my work at [${github}](https://github.com/${github})
+Get to know more of my work at my Github: [${github}](https://github.com/${github})
 `;
 
 
@@ -101,25 +101,3 @@ inquirer
 
 init();
 
-
-
-/*
-with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-
-WHEN I enter my project title
-THEN this is displayed as the title of the README
-
-WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-WHEN I choose a license for my application from a list of options
-THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-WHEN I enter my GitHub username
-THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-WHEN I enter my email address
-THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-WHEN I click on the links in the Table of Contents
-THEN I am taken to the corresponding section of the README*/
